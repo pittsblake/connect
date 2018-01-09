7667037fbe1d1ddc8736f952a0a717495df6112a
pittsblake <pittsblake1@gmail.com>
2018-01-09 10:26:59 -0500

    User can now hover over each column and the last available cell is highlighted

--- game.js
+++ game.js
@@ -2,8 +2,11 @@ class Game {
     constructor(selector) {
         this.ROWS = 6;
         this.COLS = 7;
+        this.player = 'red';
         this.selector = selector
         this.createGrid();
+        this.eventListeners();
+        
     }
 
     createGrid() {
@@ -13,10 +16,44 @@ class Game {
             const row = $('<div>').addClass('row');
             game.append(row)
             for (let y = 0; y < this.COLS; y++){
-                const col = $('<div>').addClass('col');
+                const col = $('<div>')
+                    .addClass('col empty')
+                    .attr('data-col', y)
+                    .attr('data-row', x);
                 row.append(col)
             }
         }
         console.log(game.html)
+    }
+
+    eventListeners() {
+        const game = $(this.selector);
+
+        const findLastEmptyCell = (col) => {
+            const cells = $(`.col[data-col='${col}']`)
+            //Loop through the column in revers to find the empty cell
+            for(let i = cells.length -1; i >=0; i--){
+                const tile = $(cells[i]);
+                if (tile.hasClass('empty')) {
+                    return tile;
+                }
+            }
+            console.log(cells)
+        }
+
+        // Grabs the index of each column.
+        game.on('mouseenter', '.col', function(){
+            const col = $(this).data('col')
+            const getLastCell = findLastEmptyCell(col)
+            getLastCell.addClass('next-red');
+            console.log(col)
+        })
+
+        //Leave hover fill when mouse leaves
+        game.on('mouseleave', '.col', function(){
+            const col = $(this).data('col')
+            const findLastCell = findLastEmptyCell(col)
+            findLastCell.removeClass('next-red')
+        })
     }
 }
